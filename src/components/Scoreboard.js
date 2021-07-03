9ceb7ffbb69a40f84dbd67d6374485044981a640
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-07-02 21:03:43 -0400

    word change

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -11,11 +11,11 @@ const Scoreboard = ({playerTurn, xPoints, oPoints})=> {
       <p>Currently player {playerTurn}'s turn</p>
       <div className="scoreContainer">
         <div className="score">
-          <p>Player X Points</p>
+          <p>-Player X Points-</p>
           <h1>{xPoints}</h1>
         </div>
         <div className="score">
-          <p>Player O Points</p>
+          <p>-Player O Points-</p>
           <h1>{oPoints}</h1>
         </div>
       </div>
